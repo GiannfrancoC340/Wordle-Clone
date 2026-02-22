65f8adf07a3e3de66a478599a9234c625fd44845
GiannfrancoCrovetto <gcrovetto2021@fau.edu>
2026-02-21 23:49:44 -0500

    fixed the animation

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import type { Tile as TileType } from '../../types'
 import styles from './Tile.module.css'
 
@@ -7,14 +8,25 @@ interface TileProps { // defining shape of the props it expects to receive
 }
 
 function Tile({ tile, tileIndex }: TileProps) { // JS destructuring and type annotation
-  const animationDelay = tile.state !== 'empty' && tile.state !== 'filled'
-    ? `${tileIndex * 300}ms`
-    : '0ms'
+  const [revealedState, setRevealedState] = useState(tile.state)
+  const isSubmitted = tile.state !== 'empty' && tile.state !== 'filled'
+  const delay = tileIndex * 400
+
+  useEffect(() => {
+    if (isSubmitted) {
+      const timer = setTimeout(() => {
+        setRevealedState(tile.state)
+      }, delay)
+      return () => clearTimeout(timer)
+    } else {
+      setRevealedState(tile.state)
+    }
+  }, [tile.state, isSubmitted, delay])
 
   return (
     <div
-      className={`${styles.tile} ${styles[tile.state]}`}
-      style={{ animationDelay }}
+      className={`${styles.tile} ${styles[revealedState]} ${isSubmitted ? styles.flip : ''}`}
+      style={{ animationDelay: `${delay}ms` }}
     >
       {tile.letter}
     </div>
